c4833c3b3fd8ba2820deff0b3aea2f00c46ecdff
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-12 22:35:05 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = "196371750:AAHguazIHFyFYCxzIrWt59ZsCGrsJ3jsT7I"--توکن بوت را درون " قرار دهید --
+local bot_api_key = "196371750:AAHguazIHFyFYCxzIrWt59ZsCGrsJ3jsT7I"
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
