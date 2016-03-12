10557601aaf08ba5b417511307e4c548bb821f2e
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-12 22:29:37 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = " "--توکن بوت را درون " قرار دهید --
+local bot_api_key = "196371750:AAHguazIHFyFYCxzIrWt59ZsCGrsJ3jsT7I"--توکن بوت را درون " قرار دهید --
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[
@@ -57,7 +57,7 @@ _TNX TO USE ME :)_
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {94746365}-- put your id here
+  local admins = {150575718}-- put your id here
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
